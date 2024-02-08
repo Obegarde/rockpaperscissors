4ad7fae0a508f26b715458ea944a6c8467e51441
Obegarde <5763603+Obegarde@users.noreply.github.com>
2024-02-08 10:33:39 +0100

    Add playGame function.
    String it all together so you can play 5 rounds against the computer.

--- game.js
+++ game.js
@@ -54,3 +54,37 @@ function playRound(playerSelection,computerSelection = getComputerChoice()){
     }
     }
 
+    //Make a playGame function that plays 5 games and keeps tracks of who wins.
+    function playGame(){
+    // Make two variables to hold the number of wins of each player
+        let playerScore = 0;
+        let computerScore = 0;
+    //Make a loop that runs for 5 rounds
+    for(let i = 1; i < 6; i++){
+    //make a variable to hold this rounds winner.
+        let roundWinner;
+    //Prompt the player for a choice
+        let playerChoice = prompt("What do you choose ?", "Rock, Paper or Scissors ?");
+    //then plays a round
+       roundWinner = playRound(playerChoice);
+    //tallies the score
+        if(roundWinner[4] ==='W'){
+            playerScore += 1;
+        }else if(roundWinner[4] === 'L'){
+            computerScore += 1;
+        }
+    //Depending on what playRound() returns it should write the winner
+        console.log(roundWinner);
+        console.log(`Round: ${i}\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
+    //Then restarts until 5 rounds have been played
+    }
+    //announce the final winner
+        if(playerScore === computerScore){
+            console.log(`With a score of ${playerScore} to ${computerScore} you have tied. That Should not be possible!`)
+        }else if(playerScore > computerScore){
+            console.log(`You Won with a final score of ${playerScore} versus ${computerScore}`)
+        }else if(playerScore < computerScore){
+            console.log(`You lost with a final score of ${playerScore} versus ${computerScore}`)
+        }
+    
+    }
